acc14bad8523ea7320e4a6a44f6abdf7b1bbcc52
Szilard Parrag <szilard.parrag@axoflow.com>
2025-10-03 09:31:42 +0200

    feat(etwreceiver): add validation for buffer_size value
    Signed-off-by: Szilard Parrag <szilard.parrag@axoflow.com>

--- receiver/etwreceiver/config_windows.go
+++ receiver/etwreceiver/config_windows.go
@@ -5,6 +5,8 @@
 
 package etwreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/etwreceiver"
 
+import "fmt"
+
 func (c *WindowsEtwConfig) Validate() error {
 	if _, err := c.extractProviderGUID(); err != nil {
 		if !c.IgnoreMissingProvider {
@@ -14,6 +16,12 @@ func (c *WindowsEtwConfig) Validate() error {
 	}
 	if _, err := TraceLevelFromString(c.Level); err != nil {
 		return err
+	}
+	if c.BufferSize < ETWReceiverMinimumBufferSize {
+		return fmt.Errorf("buffer_size must be at least %v (in KB)", ETWReceiverMinimumBufferSize)
+	}
+	if c.BufferSize > ETWReceiverMaximumBufferSize {
+		return fmt.Errorf("buffer_size must be at most %v (in KB)", ETWReceiverMaximumBufferSize)
 	}
 	return nil
 }
