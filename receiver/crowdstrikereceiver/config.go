1b4dc9a63dc1931e92bd6b54a63d3dc997d0afee
Szilard Parrag <szilard.parrag@axoflow.com>
2025-11-25 09:49:40 +0100

    feat(crowdstrike-receiver): switch to real implementation
    Signed-off-by: Szilard Parrag <szilard.parrag@axoflow.com>

--- receiver/crowdstrikereceiver/config.go
+++ receiver/crowdstrikereceiver/config.go
@@ -3,7 +3,36 @@
 
 package crowdstrikereceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/crowdstrikereceiver"
 
+import "time"
+
 type CrowdstrikeReceiverConfig struct {
-	ClientID     string `mapstructure:"client_id"`
+	// AccessToken is the access token used to access the CrowdStrike Falcon platform.
+	// If used, Cloud must be provided.
+	// *required* if ClientID and ClientSecret are empty.
+	AccessToken string `mapstructure:"access_token"`
+
+	// ClientID used for authentication with CrowdStrike Falcon platform.
+	// *required* if AccessToken is empty.
+	ClientID string `mapstructure:"client_id"`
+	// ClientSecret used for authentication with CrowdStrike Falcon platform.
+	// *required* if AccessToken is empty.
 	ClientSecret string `mapstructure:"client_secret"`
+
+	// MemberCID is an optional CID selector for cases when the ClientID/ClientSecret
+	// has access to multiple CIDs.
+	MemberCID string `mapstructure:"member_cid"`
+
+	// Cloud specifies the Falcon Cloud to connect to (e.g., "us-1", "us-2", "eu-1").
+	Cloud string `mapstructure:"cloud"`
+
+	// HostOverride allows to override host. Cloud will be ignored.
+	HostOverride string `mapstructure:"host_override"`
+	// BasePathOverride allows to override default base path
+	BasePathOverride string `mapstructure:"base_path_override"`
+
+	// PollInterval specifies how often to poll the CrowdStrike API for new data.
+	PollInterval *time.Duration `mapstructure:"poll_interval"`
+
+	// Debug enables debug logging of all HTTP traffic going through the API runtime.
+	Debug bool `mapstructure:"debug"`
 }
